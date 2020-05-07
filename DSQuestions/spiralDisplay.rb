e3bdc4b02aac49263eed1935eec3b0c36890f2b3
Shivang <shivang.sharma15@outlook.com>
2020-05-07 18:59:26 +0530

    added new file

--- DSQuestions/spiralDisplay.rb
+++ DSQuestions/spiralDisplay.rb
@@ -1,35 +1,56 @@
 m = ARGV[0].to_i
 n = ARGV[1].to_i
-ARGV.clear();
+ARGV.clear()
 arr = []
 
-for i in (0..m-1)
-    row=[]
-    for j in (0..n-1) 
-        val = gets.chomp.to_i;
-        row.push(val);
-
-    end
-    arr.push(row)    
+for i in (0..m - 1)
+  row = []
+  for j in (0..n - 1)
+    val = gets.chomp.to_i
+    row.push(val)
+  end
+  arr.push(row)
 end
-
 
 puts "************************************"
 
-def spDisp(arr)
-  row = 0
-  col = 0
-  # print left wall
-  for i in (row..arr.length - 1)
-    for j in (0)
-      print(arr[i][j].to_s + " ")
+def spiral_display(arr)
+  rmin = 0
+  cmin = 0
+  rmax = arr.length
+  cmax = arr[0].length
+  k = 0
+  total = arr.length * arr[0].length
+  while (k < total)
+    for i in (rmin..rmax)
+      if (k < total)
+        puts (arr[i][cmin])
+        k += 1
+      end
     end
+    cmin += 1
+    for i in (cmin..cmax)
+      if (k < total)
+        puts(arr[rmax][i])
+        k += 1
+      end
+    end
+    rmax -= 1
+    for i in (rmax).downto(rmin)
+      if (k < total)
+        puts(arr[i][cmax])
+        k += 1
+      end
+    end
+    cmax -= 1
+    for i in (cmax).downto(cmin)
+      if (k < total)
+        puts(arr[rmin][i])
+        k += 1
+      end
+    end
+    rmin += 1
   end
-  col = col +1
-        # print bottom wall
-        for i in (arr.length - 1)
-          for j in (col..arr[0].length - 1)
-            print(arr[i][j].to_s + " ")
-          end
-        end
 end
+
+spiral_display(arr)
