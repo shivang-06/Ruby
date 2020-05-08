495a3cd54c95712d6582b096a2fb359ec98a6e59
Shivang <shivang.sharma15@outlook.com>
2020-05-08 19:07:59 +0530

    added new file

---
+++ DSQuestions/dijakstra.rb
@@ -0,0 +1,36 @@
+class Edge
+  attr_accessor :nvtx, :wt
+
+  def initialize(nvtx, wt)
+    @nvtx = nvtx
+    @wt = wt
+  end
+
+  def to_s()
+    return nvtx.to_s+" @ "+wt.to_s
+  end
+
+end
+
+
+graph = [];
+
+#LHS represent vertex, RHS represent cost to reach the neighbor and neighbors.
+graph[0] = [Edge.new(1, 10), Edge.new(3, 40)]
+graph[1] = [Edge.new(0, 10), Edge.new(2, 40)]
+graph[2] = [Edge.new(1, 10), Edge.new(3, 10)]
+graph[3] = [Edge.new(0, 40), Edge.new(4, 2), Edge.new(2,10)]
+graph[4] = [Edge.new(3,2), Edge.new(6, 5), Edge.new(5,3)]
+graph[5] = [Edge.new(4, 3), Edge.new(6, 3)]
+graph[6] = [Edge.new(5,3), Edge.new(4, 5)]
+
+def display(graph)
+    for vtx in (0..graph.length-1) do
+        print vtx.to_s+"   ->  "
+        for ei in (0..graph[vtx].length-1) do
+            print graph[vtx][ei].to_s+ " , "
+        end
+        puts
+    end
+end
+display(graph)
